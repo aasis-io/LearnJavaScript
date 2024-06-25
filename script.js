b45ed65e83ba917458734c071da3f9d32bf481bf
Ashish Thapa <heyashishthapa@gmail.com>
2024-06-25 09:18:30 +0545

    Added Variables

---
+++ script.js
@@ -0,0 +1,68 @@
+// variables
+
+//var
+// var x = 90;
+// console.log(x);
+
+// var x = 180;
+// console.log(x);
+
+//let
+// let y = 90;
+// console.log(y);
+
+// cannot be redeclared
+// let y = 10;
+
+//can be updated
+// y = 20;
+// console.log(y);
+
+//const
+// const z = 10;
+// console.log(z);
+
+//cannot be redeclared
+//cannot be updated
+// z = 9;        // --- error occurs
+// console.log(z);
+
+//Date types in JavaScript
+//string
+// const output = "Hello World!";
+// console.log(typeof output);
+
+//number
+// const result = 90;
+// console.log(result);
+// console.log(typeof result);
+
+//float
+// const result =3.67;
+// console.log(result);
+// console.log(typeof result);
+
+// undefined
+// let y;
+// console.log(y);
+// console.log(typeof y);
+
+// null
+// let y = null;
+// console.log(y);
+// console.log(typeof y);
+
+// boolean
+// let y = true;
+// console.log(y);
+// console.log(typeof y);
+
+//BigInt
+// let y = 345346456445n;
+// console.log(y);
+// console.log(typeof y);
+
+// Symbol
+const y = Symbol("Hi!");
+console.log(y);
+console.log(typeof y);
